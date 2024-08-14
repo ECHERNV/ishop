dc9448ad0b104aac7d3510fc53bc49b75494610d
JohnnyWex <chernavinj@gmail.com>
2024-08-15 02:30:03 +0500

    Шаблон
    Signed-off-by: JohnnyWex <chernavinj@gmail.com>

--- app/views/layouts/ishop.php
+++ app/views/layouts/ishop.php
@@ -4,6 +4,8 @@ use chrn\View;
 /** @var $this View */
 ?>
 
-<?php $this->getPart('header'); ?>
-<?php $this->getPart('footer'); ?>
+<?php $this->getPart('/header') ?>
 
+<?php echo $this->content ?>
+
+<?php $this->getPart('footer');
